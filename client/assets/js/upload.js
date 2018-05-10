c71e616a0a0b0e7a2d4b82acc38dafcebbf62738
yasirmaulana <yasir.maulana@gmail.com>
2018-05-10 14:19:30 +0700

    menambahkan fitur upload - frontend

---
+++ client/assets/js/upload.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+    el:'#app',
+    data: {
+        caption: '',
+        imageUrl: ''
+    },
+    methods: {
+        uploadImg: function(){
+            let fileInput = document.querySelector('#image');
+            console.log(fileInput)
+
+            let formData = new FormData()
+            formData.append('caption', this.caption)
+            formData.append('image', fileInput.files[0])
+
+            axios.post('http://localhost:3000/upload',formData)
+             .then( response => {
+                 console.log(response)
+             })
+             .catch( error => {
+                 console.log('error dari upload.html',error)
+             })
+        }
+    }
+})
