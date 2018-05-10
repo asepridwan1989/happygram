ccd4d8fdb452c210f616dafe15cc56bdfd8667d6
yasirmaulana <yasir.maulana@gmail.com>
2018-05-10 15:25:40 +0700

    tes integrate hook

--- server/lib/mongoose-connection.js
+++ server/lib/mongoose-connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/db-happygram');
+// mongoose.connect('mongodb://localhost:27017/db-happygram');
+mongoose.connect(`mongodb://userHappygram:1234567890@ds117540.mlab.com:17540/happygram`);
 
 module.exports = { mongoose };
--- server/models/image.js
+++ server/models/image.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const Joke = require('../lib/joke');
 
 const imageSchema = new Schema({
     userId: String,
     imageUrl: String,
     caption: String,
-    // like: [{
-    //         type: Schema.Types.ObjectId,
-    //         ref: 'User',
-    //         required: false
-    //     }],
-    // isHappy: Boolean
 })
 
+imageSchema.pre('save', true, function(next, done) {
+    // calling next kicks off the next middleware in parallel
+    let image = this
+    Joke.getJoke().then((result) => {
+        image.caption = result.value.Joke        
+        next();
+      })
+  });
 
 let Image = mongoose.model('Images', imageSchema)
 module.exports = Image
